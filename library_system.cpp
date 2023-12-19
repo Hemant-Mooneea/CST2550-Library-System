1a4c373baf0954c5771f3a6bff68f252430c0e2b
Hemant <HM1133@live.mdx.ac.uk>
2023-12-19 11:09:01 +0400

    Added setters and getters for person

--- library_system.cpp
+++ library_system.cpp
@@ -1,3 +1,44 @@
 #include <iostream>
 #include <vector>
-#include <library_system.h>
+#include "library_system.h"
+
+std::string Person::getName()
+{
+    return this->name;
+}
+std::string Person::getEmail()
+{
+    return this->email;
+}
+std::string Person::getAddress()
+{
+    return this->address;
+}
+void Person::setName(std::string name)
+{
+    this->name = name;
+}
+void Person::setEmail(std::string email)
+{
+    this->email = email;
+}
+void Person::setAddress(std::string address)
+{
+    this->address = address;
+}
+
+Librarian::Librarian(int staffID, std::string name, std::string address,
+std::string email, int salary)
+{
+    this->staffId = staffID;
+    this->salary = salary;
+    setName(name);
+    setAddress(address);
+    setEmail(email);
+}
+
+int main()
+{
+    Librarian lib(1000, "a", "b", "gmail", 10000);
+    return 1;
+}
